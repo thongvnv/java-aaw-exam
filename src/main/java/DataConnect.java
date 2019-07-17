6e54809c98611593beb45336d881769390026efd
unknown <thongnhd00522@fpt.edu.vn>
2019-07-17 11:33:13 +0700

    Init

---
+++ src/main/java/DataConnect.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataConnect {
+
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/user", "root", "");
+			return con;
+		} catch (Exception ex) {
+			System.out.println("Database.getConnection() Error -->"
+					+ ex.getMessage());
+			return null;
+		}
+	}
+
+	public static void close(Connection con) {
+		try {
+			con.close();
+		} catch (Exception ex) {
+		}
+	}
+}
